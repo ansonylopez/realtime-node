d25033368c720600af1c4815d88a643313a45e0f
Ansony Lopez <ansonylopez@hotmail.com>
2023-11-14 22:05:57 -0600

    add socket.io to chat

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 import express from "express";
 import logger from "morgan";
 
-const port = process.env.port ?? 3000;
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+const port = process.env.port ?? 3001;
 
 const app = express();
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg)
+        io.emit('chat message', msg)
+    })
+
+})
+
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
@@ -11,7 +32,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
